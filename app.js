c10916199fbfd278ae79f5df2e2d737722925434
keyshanavii <ayassikeysha@gmail.com>
2024-10-24 14:48:37 +0700

    selesai

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 function main() {
+    const hasilDadu = [1, 5, 2, 6, 2, 3]
   
-
-
-}
-
-module.exports = main
+    let kemunculanGanjil = 0;
+    let kemunculanGenap = 0;
+  
+    for (let angka of hasilDadu){
+      if (angka % 2 !== 0){
+          kemunculanGanjil++;
+      }
+      else{
+          kemunculanGenap++;
+      }
+    }
+  console.log(`Raka mendapatkan angka ganjil sebanyak ${kemunculanGanjil} kali dan angka genap sebanyak ${kemunculanGenap} kali `)
+  
+  }
+  main()
+  
+  module.exports = main
